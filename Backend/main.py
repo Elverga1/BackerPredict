17773ea7e31406fbf0cbc1eeef7898128f0f05ea
Elverga1 <onofree484@gmail.com>
2026-06-16 11:18:01 -0600

    Realice la conexion del frontend y backend, faltan los estilos(CSS)

--- Backend/main.py
+++ Backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from auth import router as auth_router
 from ventas import router as ventas_router
 from produccion import router as produccion_router
@@ -6,6 +7,16 @@ from reportes import router as reportes_router
 from usuarios import router as usuarios_router
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(auth_router)
 app.include_router(ventas_router)
 app.include_router(produccion_router)
